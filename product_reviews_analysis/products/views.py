edbc52fc804236a0fd30e6fb4725d681d82cd450
Navin Gohite <navingohite07@gmail.com>
2022-05-30 18:15:05 +0530

    updated first working version

--- product_reviews_analysis/products/views.py
+++ product_reviews_analysis/products/views.py
@@ -9,23 +9,34 @@ def index(request):
     products = [product for product in Product.objects.all()]
 
     if request.method == 'POST':
-        print("form is not valid")
+        review_all = ""
         product_id = int(request.POST["product_id"])
         reviews = Review.objects.filter(product_id=product_id)
         for review in reviews:
+            review_all = review_all + review.review
             score = sentiment_analysis(review.review)
             review.__setattr__("score_compound", score["compound"])
             review.__setattr__("score_pos", score["pos"])
             review.__setattr__("score_neg", score["neg"])
             review.__setattr__("score_neu", score["neu"])
+        
+        selected_product = Product.objects.filter(id=product_id)
+        score = sentiment_analysis(review_all)
+        selected_product.__setattr__("score_compound", score["compound"])
+        selected_product.__setattr__("score_pos", score["pos"])
+        selected_product.__setattr__("score_neg", score["neg"])
+        selected_product.__setattr__("score_neu", score["neu"])
     else:
         reviews = Review.objects.all()
-        product_id =products[0].id
-        scores = {}
+        selected_product =products[0]
+        selected_product.__setattr__("score_compound", 0)
+        selected_product.__setattr__("score_pos", 0)
+        selected_product.__setattr__("score_neg", 0)
+        selected_product.__setattr__("score_neu", 0)
 
     context = {
         "products": products,
-        "selected_product_id": product_id,
+        "selected_product": selected_product,
         "reviews" : reviews,
     }
 
